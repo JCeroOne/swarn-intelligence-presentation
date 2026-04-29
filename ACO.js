e1164cc727cdad2e13b9ddfdf8e843d91e56ccbf
JCeroOne <jceroone@gmail.com>
2026-04-28 22:20:04 -0600

    Update ACO.js

--- ACO.js
+++ ACO.js
@@ -41,6 +41,9 @@ class Grid {
     getCell(x, y){
         return this.#cells[`${x} ${y}`] ? this.#cells[`${x} ${y}`] : null;
     }
+    getCell(id) {
+        return this.#cells[id] ? this.#cells[id] : null;
+    }
     print(){
         let display = "";
         for(let y = 0; y < this.height; y++){
@@ -64,6 +67,9 @@ class GridCell {
         this.y = y;
         this.type = type;
         this.pheromones = 0;
+    }
+    addPheromones(ph){
+        this.pheromones += ph;
     }
     Step(){
         this.pheromones *= 0.9;
@@ -90,13 +96,22 @@ class Ant {
         this.x = x;
         this.y = y;
         this.path = [];
+        this.finished = false;
+        this.found_food = false;
+    }
+    #validateCell(x, y){
+        const cell = this.grid.getCell(x, y);
+        if(!cell) return false;
+        if(cell.type == "obstacle") return false;
+        if(this.path.indexOf(`${x} ${y}`) != -1) return false;
+        return true;
     }
     calcProbs(){
         const cells = [];
-        if(this.path.indexOf([this.x, this.y + 1]) == -1) cells.push({cell: this.grid.getCell(this.x, this.y + 1), prob: 0});
-        if(this.path.indexOf([this.x + 1, this.y]) == -1) cells.push({cell: this.grid.getCell(this.x + 1, this.y), prob: 0});
-        if(this.path.indexOf([this.x - 1, this.y]) == -1) cells.push({cell: this.grid.getCell(this.x - 1, this.y), prob: 0});
-        if(this.path.indexOf([this.x, this.y - 1]) == -1) cells.push({cell: this.grid.getCell(this.x, this.y - 1), prob: 0});
+        if(this.#validateCell(this.x, this.y + 1)) cells.push({cell: this.grid.getCell(this.x, this.y + 1), prob: 0});
+        if(this.#validateCell(this.x + 1, this.y)) cells.push({cell: this.grid.getCell(this.x + 1, this.y), prob: 0});
+        if(this.#validateCell(this.x - 1, this.y)) cells.push({cell: this.grid.getCell(this.x - 1, this.y), prob: 0});
+        if(this.#validateCell(this.x, this.y - 1)) cells.push({cell: this.grid.getCell(this.x, this.y - 1), prob: 0});
         const total = cells.reduce((total, current) => total + Math.max(0.1, current.cell.pheromones), 0);
         cells.forEach(c => {
             c.prob = Math.max(0.1, c.cell.pheromones) / total;
@@ -104,8 +119,18 @@ class Ant {
         return cells;
     }
     move(){
-        this.path.push([this.x, this.y]);
+        this.path.push(`${this.x} ${this.y}`);
+        if(this.path.length >= MAX_PATH_LENGTH) { // Didn't find the food: Won't lay pheromones.
+            this.finished = true;
+            this.found_food = false;
+            return;
+        }
         const cells = this.calcProbs();
+        if(cells.filter(c => c.cell.type == "food").length > 0) { // Found the food: Will lay pheromones.
+            this.finished = true;
+            this.found_food = true;
+            return;
+        }
         let next = Math.random();
         let nextCell = null;
         let cumulative = 0;
@@ -120,6 +145,10 @@ class Ant {
         this.y = nextCell.y;
     }
     layPheromones(){
-
+        const phPerCell = 1 / this.path.length;
+        this.path.forEach(c => {
+            const cell = this.grid.getCell(c);
+            cell.addPheromones(phPerCell);
+        })
     }
 }
